7b192eae5aae89322afa7ad81ffde6f8b19fddee
SIonut0122 <34577347+SIonut0122@users.noreply.github.com>
2018-06-28 15:34:33 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# restaurant-review
-Restaurant Review - Udacity
+This is a project developed by Udacity and Google Front End Web Developer Scholarship. 
+In this project i had to convert a static design to be responsive on different sized displays. Was required to make the website accessibile for screen reader, and for the las part it was added a service worker to make the website offline first.
+
+
+For this project i used:
+
+- ServiceWorker - to make the website work offline.
+- ARIA Roles - to make the website accessibile for the screen reader.
+- MapBox application - to mark all needed restaurants on it.
+
+Install:
+
+- Python2 or python3 installed on your pc.
+- webbrowser on your pc.
+
+
+Usage:
+
+1.In root folder, start up a simple HTTP server to serve up the site files on your local computer. Python has some simple tools to do this, and you don't even need to know Python. For most people, it's already installed on your computer.
+In a terminal, check the version of Python you have: python -V. If you have Python 2.x, spin up the server with python -m SimpleHTTPServer 8000 (or some other port, if port 8000 is already in use.) For Python 3.x, you can use python3 -m http.server 8000. If you don't have Python installed, navigate to Python's website to download and install the software.
+
+With your server running, visit the site: http://localhost:8000, and look around for a bit to see what the current experience looks like.
+Explore the provided code, and make start making a plan to implement the required features in three areas: responsive design, accessibility and offline use.
+Write code to implement the updates to get this site on its way to being a mobile-ready website.
---
+++ sw.js
@@ -0,0 +1,82 @@
+
+var cacheName = 'r-v1';
+
+
+// data to catch
+
+let paths = [
+    '/',
+    './index.html',
+    './restaurant.html?',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/css/styles.css',
+    '/data/restaurants.json',
+     '/?homescreen=1',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+    ];
+
+
+// Install and create a new cache.
+
+self.addEventListener('install', function(e) {
+  console.log('Installing - ServiceWorker');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('Caching - ServiceWorker');
+      return cache.addAll(paths);
+    })
+    .then(function() {
+      console.log('Installation complete');
+    })
+  );
+});
+
+
+
+// Feth from the network.
+
+self.addEventListener('fetch', function(e) {
+  console.log('Fetch - ServiceWorker');
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+
+
+
+// Activate new cache and delete the old cache.
+
+self.addEventListener('activate', function(event) {
+    console.log('Activate new cache');
+    
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    return cacheName.startsWith('r-') && cacheName != version;
+                }).map(function(cacheName) {
+                    return cache.delete(cacheName);
+                })
+               )
+             }).catch(function (error) {
+            // Log this if there is no cache on first run.
+
+            console.log('No old cache to delete');
+            return
+        })
+       );
+     });
