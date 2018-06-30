f036219a33ae7146cd73cc99cab7b31c6094fb15
SIonut0122 <34577347+SIonut0122@users.noreply.github.com>
2018-06-30 13:11:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,97 +1,74 @@
+const currentCache = 'restaurantsCache-v1';
 
-const cacheName = 'review-cache-v1';
-
-
-// data to catch
-
-const paths = [
-  './',
-  './index.html',
-  './restaurant.html',
-  './restaurant.html?id=1',
-  './restaurant.html?id=2',
-  './restaurant.html?id=3',
-  './restaurant.html?id=4',
-  './restaurant.html?id=5',
-  './restaurant.html?id=6',
-  './restaurant.html?id=7',
-  './restaurant.html?id=8',
-  './restaurant.html?id=9',
-  './restaurant.html?id=10',
-  './css/styles.css',
-  './css/responsive.css',
-  './js/main.js',
-  './js/dbhelper.js',
-  './js/script.js',
-  './js/restaurant_info.js',
-  './data/restaurants.json',
-  './img/1.jpg',
-  './img/2.jpg',
-  './img/3.jpg',
-  './img/4.jpg',
-  './img/5.jpg',
-  './img/6.jpg',
-  './img/7.jpg',
-  './img/8.jpg',
-  './img/9.jpg',
-  './img/10.jpg',
-  './img/favicon.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css',
-  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
-];
-
-// Install and create a new cache.
-
-self.addEventListener('install', function(e) {
-  console.log('Installing - ServiceWorker');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('Caching - ServiceWorker');
-      return cache.addAll(paths);
-    })
-    .then(function() {
-      console.log('Installation complete');
-    })
-  );
-});
-
-
-
-// Feth from the network.
-
-self.addEventListener('fetch', function(e) {
-  console.log('Fetch - ServiceWorker');
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
-
-
-
-
-// Activate new cache and delete the old cache.
-
-self.addEventListener('activate', function(event) {
-    console.log('Activate new cache');
-    
+//service worker install event and caching while installing 
+self.addEventListener('install', event => {
+    //urls to be cached
+    const urlsToCache = [
+        './',
+        './index.html',
+        './restaurant.html',
+        './restaurant.html?id=1',
+        './restaurant.html?id=2',
+        './restaurant.html?id=3',
+        './restaurant.html?id=4',
+        './restaurant.html?id=5',
+        './restaurant.html?id=6',
+        './restaurant.html?id=7',
+        './restaurant.html?id=8',
+        './restaurant.html?id=9',
+        './restaurant.html?id=10',
+        './css/styles.css',
+        './data/restaurants.json',
+        './js/main.js',
+        './js/restaurant_info.js',
+        './js/dbhelper.js',
+        './img/1.jpg',
+        './img/2.jpg',
+        './img/3.jpg',
+        './img/4.jpg',
+        './img/5.jpg',
+        './img/6.jpg',
+        './img/7.jpg',
+        './img/8.jpg',
+        './img/9.jpg',
+        './img/10.jpg',
+        './img/404.png',
+        './img/favicon.png',
+        'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
+    ];
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    return cacheName.startsWith('review-') && cacheName != version;
-                }).map(function(cacheName) {
-                    return cache.delete(cacheName);
-                })
-               )
-             }).catch(function (error) {
-            // Log this if there is no cache on first run.
-
-            console.log('No old cache to delete');
-            return
+        caches.open(currentCache).then(cache => cache.addAll(urlsToCache)).catch(error => {
+            console.log(error);
         })
-       );
-     });
+    );
+    
+});
 
+//service worker activate event and deleting older caches while activating
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => Promise.all(
+            cacheNames.filter(cacheName => cacheName !== currentCache).map(cacheName => caches.delete(cacheName))
+        ))
+    );
+});
+
+/*service worker fetch event and respond with cache if there is something in cache.
+If not responding from network. If the response status is 404 then respond with 404 image*/
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if (response) return response;
+            return fetch(event.request).then(response => {
+                if (response.status === 404) {
+                    //free download and personal use png from https://pngtree.com/freepng/404-error-vector_2871439.html
+                    return fetch('./img/404.png');
+                }
+                return response;
+            })
+        })
+    );
+
+});
